38c45467af61378906559f0b9c7095a44c49e865
Brilliantsyach <164490598+Brilliantsyach@users.noreply.github.com>
2024-12-15 19:26:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Created by Scorpio✧⁠◝⁠(⁠⁰⁠▿⁠⁰⁠)⁠◜⁠✧
+
+alert ("Please enter your date of birth to find your zodiac sign😄😊🤟")
+alert ("Don't forget to comment ,like and follow me👍👍")
+alert ("Appreciate your sign 😜😘")
